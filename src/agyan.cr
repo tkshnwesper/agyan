9fac04e959a53099ddcdb8fc48c6309035855f2e
Saurabh Machave <unknown7bolt@gmail.com>
2021-04-08 20:17:13 +0530

    take optional name param for mock class

--- src/agyan.cr
+++ src/agyan.cr
@@ -1,8 +1,8 @@
 module Agyan
   VERSION = "0.1.0"
 
-  macro mock_class(type)
-    class Mock{{ type.id }} < {{ type.id }}
+  macro mock_class(type, name = nil)
+    class {% if name %} {{name}} {% else %} Mock{{type.id}} {% end %} < {{ type.id }}
     end
   end
 end
