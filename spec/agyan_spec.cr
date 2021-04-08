3090b66499279003186f2d39dd56b66acafc031e
Saurabh Machave <unknown7bolt@gmail.com>
2021-04-08 19:58:42 +0530

    create class mock

--- spec/agyan_spec.cr
+++ spec/agyan_spec.cr
@@ -1,9 +1,18 @@
 require "./spec_helper"
 
-describe Agyan do
-  # TODO: Write tests
+include Agyan
 
-  it "works" do
-    false.should eq(true)
+class ClassToBeMocked
+  def method_with_no_parameters : Int32
+    456
+  end
+end
+
+mock_class(ClassToBeMocked)
+
+describe Agyan do
+  it "creates a mock" do
+    mock = MockClassToBeMocked.new
+    mock.responds_to?(:method_with_no_parameters).should eq(true)
   end
 end
--- src/agyan.cr
+++ src/agyan.cr
@@ -1,6 +1,8 @@
-# TODO: Write documentation for `Agyan`
 module Agyan
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  macro mock_class(type)
+    class Mock{{ type.id }} < {{ type.id }}
+    end
+  end
 end
