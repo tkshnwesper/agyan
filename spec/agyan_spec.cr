ecfc5d4b4ebc9a2b4912fb1f6b79892bf3d13bd2
Saurabh Machave <unknown7bolt@gmail.com>
2021-04-10 09:17:45 +0530

    refactor test method name

--- spec/agyan_spec.cr
+++ spec/agyan_spec.cr
@@ -3,10 +3,10 @@ require "./spec_helper"
 include Agyan
 
 class ClassToBeMocked
-  def method_with_no_parameters : Int32
+  def some_method : Int32
     456
   end
-  def method_with_no_parameters(num : Int32) : Int32
+  def some_method(num : Int32) : Int32
     num * 456
   end
 end
@@ -18,39 +18,39 @@ describe Agyan do
   describe "mock class" do
     it "creates a mock" do
       mock = MockClassToBeMocked.new
-      mock.responds_to?(:method_with_no_parameters).should eq(true)
+      mock.responds_to?(:some_method).should eq(true)
     end
 
     it "creates a mock that takes an optional name parameter for mock class" do
       mock = MockedClass.new
-      mock.responds_to?(:method_with_no_parameters).should eq(true)
+      mock.responds_to?(:some_method).should eq(true)
     end
 
     it "returns a value specified by user" do
       mock = MockedClass.new
       return_value = 123
-      MockedClass.on(mock, :method_with_no_parameters).with().then_return(return_value)
-      mock.method_with_no_parameters.should eq(return_value)
+      MockedClass.on(mock, :some_method).with().then_return(return_value)
+      mock.some_method.should eq(return_value)
     end
 
     it "returns different values when called different times" do
       mock = MockedClass.new
       return_value = 123
       return_value_two = 110
-      MockedClass.on(mock, :method_with_no_parameters).with().then_return(return_value)
-      MockedClass.on(mock, :method_with_no_parameters).with().then_return(return_value_two)
-      mock.method_with_no_parameters.should eq(return_value)
-      mock.method_with_no_parameters.should eq(return_value_two)
+      MockedClass.on(mock, :some_method).with().then_return(return_value)
+      MockedClass.on(mock, :some_method).with().then_return(return_value_two)
+      mock.some_method.should eq(return_value)
+      mock.some_method.should eq(return_value_two)
     end
 
     it "mocks a overridden method" do
       mock = MockedClass.new
       return_value = 123
       return_value_two = 110
-      MockedClass.on(mock, :method_with_no_parameters).with().then_return(return_value)
-      MockedClass.on(mock, :method_with_no_parameters).with(110).then_return(return_value_two)
-      mock.method_with_no_parameters.should eq(return_value)
-      mock.method_with_no_parameters(110).should eq(return_value_two)
+      MockedClass.on(mock, :some_method).with().then_return(return_value)
+      MockedClass.on(mock, :some_method).with(110).then_return(return_value_two)
+      mock.some_method.should eq(return_value)
+      mock.some_method(110).should eq(return_value_two)
     end
   end
 end
